54d585dd241739c8ce80a911593966cdabb60995
simonesiega <simonesiega1@gmail.com>
2024-05-08 10:16:09 +0200

    Update support jdk 21 + init characterFrame
    Cambiando le lambda ora il codice risulta supportato anche da jdk 21 + aggiunti controlli su ability Frame

--- src/nobinobi/Main.java
+++ src/nobinobi/Main.java
@@ -1,10 +1,14 @@
 package nobinobi;
 
 import nobinobi.obj.frame.AbilityFrame;
+import nobinobi.obj.frame.CharacterFrame;
 import nobinobi.obj.frame.IntroductionFrame;
 
 public class Main{
     public static void main(String[] args)  {
+
+        CharacterFrame b = new CharacterFrame();
+        b.setVisible(true);
 
         IntroductionFrame a = new IntroductionFrame();
         a.setVisible(true);
